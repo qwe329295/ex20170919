067d108aeb50ea244e379630818546f891a4340a
蕭佑誠 <105021028@live.asia.edu.tw>
2017-09-19 12:30:57 +0800

    summit
    ex20170919

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -6,18 +6,33 @@ import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame {
     private Button btnExit =new Button("exit");
+    private Button btnAdd =new Button("Add");
+    private Label lab =new Label("0");
+    private int n=0;
     public MainFrame(){
         initComp();
     }
     private void initComp(){
         this.setBounds(100,120,400,300);
         this.add(btnExit);
+        this.add(btnAdd);
+        this.add(lab);
         this.setLayout(null);
-        btnExit.setBounds(50,80,80,30);
+        btnExit.setBackground(new Color(255, 133, 59));
+        btnExit.setBounds(150,80,80,30);
+        btnAdd.setBounds(150,110,80,30);
+        lab.setBounds(150,200,80,30);
         btnExit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
+            }
+        });
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+              n++;
+              lab.setText(Integer.toString(n));
             }
         });
         this.addWindowListener(new WindowAdapter() {
